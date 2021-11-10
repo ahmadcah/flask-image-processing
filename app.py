5d2008157171eaa6f3f06fef8ed40dbb5c8362e5
ahmadcah <hanif_a13@yahoo.com>
2021-11-10 10:16:35 +0700

    Task Done

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # web-app for API image manipulation
 from flask import Flask, request, render_template, send_from_directory
 import os
-from PIL import Image
 import cv2
 from werkzeug.utils import redirect
 import process as process
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 
 @app.after_request
 def add_header(r):
@@ -17,17 +17,19 @@ def add_header(r):
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
 
+
 # default access page
 @app.route("/")
 def main():
     return render_template('proses.html')
+
 
 # upload selected image and forward to processing page
 @app.route("/proses1", methods=['POST'])
 def proses1():
     target = os.path.join(APP_ROOT, 'static/images/')
     filename = request.files['file']
-    print('filename : ',filename)
+    print('filename : ', filename)
 
     # create image directory if not found
     if not os.path.isdir(target):
@@ -36,8 +38,8 @@ def proses1():
     # save file
     data = os.path.join(target, "query.jpg")
     filename.save(data)
-    #img = cv2.imread(data, cv2.IMREAD_GRAYSCALE)
-    img=data
+    # img = cv2.imread(data, cv2.IMREAD_GRAYSCALE)
+    img = data
     print(img)
     # check mode
     if 'otsu' in request.form.get('select_thresholding'):
@@ -46,27 +48,49 @@ def proses1():
         mode = 'niblack'
     elif 'sauvola' in request.form.get('select_thresholding'):
         mode = 'sauvola'
-        
-    #process
+    elif 'medianBlur15' in request.form.get('select_thresholding'):
+        mode = 'medianBlur15'
+    elif 'morphology7x7' in request.form.get('select_thresholding'):
+        mode = 'morphology7x7'
+    elif 'gradientOrientation' in request.form.get('select_thresholding'):
+        mode = 'gradientOrientation'
+    elif 'cannyWide' in request.form.get('select_thresholding'):
+        mode = 'cannyWide'
+
+    # process
     if mode == 'otsu':
         print('start otsu')
         img_res = process.otsu_thresh(img)
         print(img_res)
-        cv2.imwrite("/".join([target, 'result.jpg']),img_res)
+        cv2.imwrite("/".join([target, 'result.jpg']), img_res)
     elif mode == 'niblack':
         img_res = process.niblack_thresh(img)
-        cv2.imwrite("/".join([target, 'result.jpg']),img_res)
+        cv2.imwrite("/".join([target, 'result.jpg']), img_res)
     elif mode == 'sauvola':
         img_res = process.sauvola_thresh(img)
-        cv2.imwrite("/".join([target, 'result.jpg']),img_res)
+        cv2.imwrite("/".join([target, 'result.jpg']), img_res)
+    elif mode == 'medianBlur15':
+        img_res = process.medianBlur15(img)
+        cv2.imwrite("/".join([target, 'result.jpg']), img_res)
+    elif mode == 'morphology7x7':
+        img_res = process.morphology7x7(img)
+        cv2.imwrite("/".join([target, 'result.jpg']), img_res)
+    elif mode == 'gradientOrientation':
+        img_res = process.gradientOrientation(img)
+        cv2.imwrite("/".join([target, 'result.jpg']), img_res)
+    elif mode == 'cannyWide':
+        img_res = process.cannyWide(img)
+        cv2.imwrite("/".join([target, 'result.jpg']), img_res)
 
     # forward to processing page
     return redirect('/')
+
 
 # retrieve file from 'static/images' directory
 @app.route('/static/images/<filename>')
 def send_image(filename):
     return send_from_directory("static/images", filename)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 import cv2
-from numpy.core.fromnumeric import argsort
-from skimage.filters import threshold_otsu, threshold_niblack, threshold_sauvola
+import numpy as np
+
 
 def otsu_thresh(img):
     img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
@@ -10,31 +10,60 @@ def otsu_thresh(img):
 def niblack_thresh(img):
     img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
     res = img.copy()
-    thresh_niblack = threshold_niblack(img, window_size=25, k=0.8)
-    thresh_niblack = cv2.ximgproc.niBlackThreshold(img, maxValue=255, type=cv2.THRESH_BINARY_INV, blockSize=2*11+1, k=-0.2, binarizationMethod=cv2.ximgproc.BINARIZATION_NICK)
+    thresh_niblack = cv2.ximgproc.niBlackThreshold(img, maxValue=255, type=cv2.THRESH_BINARY_INV, blockSize=2 * 11 + 1,
+                                                   k=-0.2, binarizationMethod=cv2.ximgproc.BINARIZATION_NICK)
 
     binary_niblack = img > thresh_niblack
 
     for i in range(res.shape[0]):
         for j in range(res.shape[1]):
-            if binary_niblack[i][j]==True:
-                res[i][j]=255
+            if binary_niblack[i][j] == True:
+                res[i][j] = 255
             else:
-                res[i][j]=0
+                res[i][j] = 0
     return res
+
 
 def sauvola_thresh(img):
     img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
     res = img.copy()
-    thresh_sauvola = threshold_sauvola(img, window_size=25)
-    thresh_sauvola = cv2.ximgproc.niBlackThreshold(img, maxValue=255, type=cv2.THRESH_BINARY_INV, blockSize=2*11+1, k=-0.2, binarizationMethod=cv2.ximgproc.BINARIZATION_SAUVOLA)
+    thresh_sauvola = cv2.ximgproc.niBlackThreshold(img, maxValue=255, type=cv2.THRESH_BINARY_INV, blockSize=2 * 11 + 1,
+                                                   k=-0.2, binarizationMethod=cv2.ximgproc.BINARIZATION_SAUVOLA)
 
     binary_sauvola = img > thresh_sauvola
 
     for i in range(res.shape[0]):
         for j in range(res.shape[1]):
-            if binary_sauvola[i][j]==True:
-                res[i][j]=255
+            if binary_sauvola[i][j] == True:
+                res[i][j] = 255
             else:
-                res[i][j]=0
+                res[i][j] = 0
     return res
+
+
+def medianBlur15(img):
+    image = cv2.imread(img)
+    return cv2.medianBlur(image, 15)
+
+
+def morphology7x7(img):
+    image = cv2.imread(img)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+    return cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel)
+
+
+def gradientOrientation(img):
+    image = cv2.imread(img)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    gX = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+    gY = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+    return np.arctan2(gY, gX) * (180 / np.pi) % 180
+
+
+def cannyWide(img):
+    image = cv2.imread(img)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    return cv2.Canny(blurred, 10, 200)
